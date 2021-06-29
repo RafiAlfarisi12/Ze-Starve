3dfadfcaa0c8d2d78e5ef0ac58a2c578eea97323
Fauzan Reza <66084646+fauzanreza@users.noreply.github.com>
2021-06-30 01:17:48 +0700

    Add files via upload
    Add Table Driven Barang Bantuan

---
+++ Ze-Starve/BarangBantuan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Fauzan Reza Arnanda
+//1302194003
+    
+namespace Ze_Starve.Penerima_Bantuan
+{
+    class BarangBantuan
+    {
+        public enum Barang
+        {
+            NasiLiwet,
+            NasiPadang,
+            LontongSayur,
+            KupatTahu,
+            SemurDaging,
+            NasiGudeg,
+            NasiLemak,
+            BuburAyam,
+            MieAyam,
+            Ketoprak,
+            PecelLele,
+            kolak,
+            NasiSateMaranggi
+
+        };
+        public static string getKodeBarang(Barang inputbarang)
+        {
+            string[] codebrg = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00"};
+
+            return codebrg[(int)inputbarang];
+        }
+
+    }
+}
