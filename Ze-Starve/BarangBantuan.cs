5c1b0a42891176d400cde2d79367929dd534bb29
Fauzan Reza <66084646+fauzanreza@users.noreply.github.com>
2021-07-02 00:09:55 +0700

    Update BarangBantuan.cs

--- Ze-Starve/BarangBantuan.cs
+++ Ze-Starve/BarangBantuan.cs
@@ -11,25 +11,34 @@ namespace Ze_Starve
 {
     class BarangBantuan
     {
-        public enum Barang
+         public enum Barang
         {
             NasiLiwet,
             NasiPadang,
             LontongSayur,
             KupatTahu,
+            TepungTerigu,
             SemurDaging,
             NasiGudeg,
+            BerasPutih,
             NasiLemak,
             BuburAyam,
             MieAyam,
+            BerasKetan,
             Ketoprak,
             PecelLele,
+            NasiKuning,
+            BerasMerah,
             kolak,
-            NasiSateMaranggi
+            NasiSateMaranggi,
+            MieAceh,
+            NasiTimbelKomplit
+
         };
         public static string getKodeBarang(Barang inputbarang)
         {
-            string[] codebrg = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00"};
+          string[] codebrg = { "A00", "B00", "C00", "D00", "E00", "F00", "G00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00", "P00", "Q00", "R00", "S00", "T00" };
+
 
             return codebrg[(int)inputbarang];
         }
