58f643651012015d13e49a3b7c82af294e6c2c1e
Fauzan Reza <66084646+fauzanreza@users.noreply.github.com>
2021-07-02 00:12:38 +0700

    Add files via upload

---
+++ Ze-Starve/DaftarAkunPenerima.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ze_Starve
+{
+    public partial class DaftarAkunPenerima : Form
+    {
+        KoneksiDatabase db = new KoneksiDatabase();
+        public DaftarAkunPenerima()
+        {
+            InitializeComponent();
+        }
+
+        private void DaftarAkunPenerima_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
