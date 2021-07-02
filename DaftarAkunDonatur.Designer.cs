562032ae69a7d7b193bd1c4186371d06a02ba1e4
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2021-07-02 23:04:59 +0900

    Final DaftarAkunDonatur

---
+++ DaftarAkunDonatur.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace Ze_Starve
+{
+    partial class DaftarAkunDonatur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pekerjaanEntry = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nikEntry = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passEntry = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.userEntry = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nbelakangEntry = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ndepanEntry = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.gajiEntry = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(123)))), ((int)(((byte)(131)))), ((int)(((byte)(235)))));
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.gajiEntry);
+            this.panel1.Controls.Add(this.resetButton);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.pekerjaanEntry);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.nikEntry);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.passEntry);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.userEntry);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.nbelakangEntry);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.ndepanEntry);
+            this.panel1.Controls.Add(this.backButton);
+            this.panel1.Controls.Add(this.nextButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 524);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.resetButton.Location = new System.Drawing.Point(588, 386);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(97, 41);
+            this.resetButton.TabIndex = 16;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(369, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 23);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Pekerjaan";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pekerjaanEntry
+            // 
+            this.pekerjaanEntry.Location = new System.Drawing.Point(374, 183);
+            this.pekerjaanEntry.Name = "pekerjaanEntry";
+            this.pekerjaanEntry.Size = new System.Drawing.Size(311, 22);
+            this.pekerjaanEntry.TabIndex = 13;
+            this.pekerjaanEntry.TextChanged += new System.EventHandler(this.pekerjaanEntry_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(369, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(276, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Nomor Induk Kependudukan (KTP)";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // nikEntry
+            // 
+            this.nikEntry.Location = new System.Drawing.Point(374, 125);
+            this.nikEntry.Name = "nikEntry";
+            this.nikEntry.Size = new System.Drawing.Size(311, 22);
+            this.nikEntry.TabIndex = 11;
+            this.nikEntry.TextChanged += new System.EventHandler(this.nikEntry_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(16, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Password";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // passEntry
+            // 
+            this.passEntry.Location = new System.Drawing.Point(21, 307);
+            this.passEntry.Name = "passEntry";
+            this.passEntry.Size = new System.Drawing.Size(311, 22);
+            this.passEntry.TabIndex = 9;
+            this.passEntry.TextChanged += new System.EventHandler(this.passEntry_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(14, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(148, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Username / Gmail";
+            // 
+            // userEntry
+            // 
+            this.userEntry.Location = new System.Drawing.Point(19, 249);
+            this.userEntry.Name = "userEntry";
+            this.userEntry.Size = new System.Drawing.Size(311, 22);
+            this.userEntry.TabIndex = 7;
+            this.userEntry.TextChanged += new System.EventHandler(this.userEntry_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(14, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nama Belakang";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // nbelakangEntry
+            // 
+            this.nbelakangEntry.Location = new System.Drawing.Point(19, 183);
+            this.nbelakangEntry.Name = "nbelakangEntry";
+            this.nbelakangEntry.Size = new System.Drawing.Size(311, 22);
+            this.nbelakangEntry.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(14, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nama Depan";
+            // 
+            // ndepanEntry
+            // 
+            this.ndepanEntry.Location = new System.Drawing.Point(19, 118);
+            this.ndepanEntry.Name = "ndepanEntry";
+            this.ndepanEntry.Size = new System.Drawing.Size(311, 22);
+            this.ndepanEntry.TabIndex = 3;
+            this.ndepanEntry.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // backButton
+            // 
+            this.backButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.backButton.Location = new System.Drawing.Point(12, 455);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(97, 41);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "Kembali";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.nextButton.Location = new System.Drawing.Point(588, 455);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(97, 41);
+            this.nextButton.TabIndex = 1;
+            this.nextButton.Text = "Selesai";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buat Akun Donatur";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(368, 218);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(180, 23);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Penghasilan Per-Bulan";
+            // 
+            // gajiEntry
+            // 
+            this.gajiEntry.Location = new System.Drawing.Point(373, 249);
+            this.gajiEntry.Name = "gajiEntry";
+            this.gajiEntry.Size = new System.Drawing.Size(311, 22);
+            this.gajiEntry.TabIndex = 17;
+            // 
+            // DaftarAkunDonatur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 521);
+            this.Controls.Add(this.panel1);
+            this.Name = "DaftarAkunDonatur";
+            this.Text = "DaftarAkunDonatur";
+            this.Load += new System.EventHandler(this.DaftarAkunDonatur_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox ndepanEntry;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox pekerjaanEntry;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox nikEntry;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox passEntry;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox userEntry;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nbelakangEntry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox gajiEntry;
+    }
+}
---
+++ DaftarAkunDonatur.cs
@@ -0,0 +1,147 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Ze_Starve
+{
+    public partial class DaftarAkunDonatur : Form
+    {
+        // Relasi ke database.
+        KoneksiDatabase db = new KoneksiDatabase();
+
+        public DaftarAkunDonatur()
+        {
+            InitializeComponent();
+        }
+        private void DaftarAkunDonatur_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pekerjaanEntry_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void passEntry_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userEntry_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nikEntry_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Mengatur tombol reset untuk mengosongkan text entry dan menonaktifkan pilihan gaji.
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            ndepanEntry.Clear();
+            nbelakangEntry.Clear();
+            userEntry.Clear();
+            passEntry.Clear();
+            nikEntry.Clear();
+            pekerjaanEntry.Clear();
+            gajiEntry.Clear();
+        }
+
+        // Mengatur tombol selesai untuk menyelesaikan pendaftaran akun.
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            // Kondisi 1 : Jika nik tidak sama dengan 16 karakter.
+            if (nikEntry.Text.Length != 16)
+            {
+                MessageBox.Show("Nomor NIK anda tidak valid");
+            }
+            // Kondisi 2 : Jika masih ada yang kosong.
+            else if (pekerjaanEntry.Text.Equals("") || nikEntry.Text.Equals("") || passEntry.Text.Equals("") ||
+                    userEntry.Text.Equals("") || passEntry.Text.Equals("") || ndepanEntry.Text.Equals("") || nbelakangEntry.Text.Equals("") ||
+                    gajiEntry.Text.Equals(""))
+            {
+                MessageBox.Show("Data Anda masih kosong");
+            }
+            // Kondisi 3 : Jika password kurang dari 6 karakter.
+            else if (passEntry.Text.Length < 6)
+            {
+                MessageBox.Show("Password minimal 6 karakter");
+            }
+            // Kondisi 4 : Kondisi 1, 2, dan 3 tidak terpenuhi.
+            else if (nikEntry.Text.Length == 16 && !pekerjaanEntry.Text.Equals("") && passEntry.Text.Length >= 6)
+            {
+                // Teknik 3 : API beserta desainnya.
+                // Mengirim data ke database sql untuk mendaftarkan akun baru.
+                db.OpenConnection();
+                MySqlCommand command = new MySqlCommand("INSERT INTO donatur(id_donatur, namaD, namaB, username, pass, nik, pekerjaan, gaji) " +
+                "VALUES(NULL, '" + ndepanEntry.Text + "', '" + nbelakangEntry.Text + "', '" + userEntry.Text + "', '" +
+                "" + passEntry.Text + "', '" + nikEntry.Text + "', '" + pekerjaanEntry.Text + "', '" + gajiEntry.Text + "')", db.GetConnection());
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Selamat, Akun anda berhasil didaftarkan");
+                    new HalamanUtamaDonatur().Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Daftar akun error. Periksa kembali data Anda");
+                }
+                db.CloseConnection();
+            }
+        }
+        // Mengatur kembali untuk membatalkan pendaftaran akun.
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            new DaftarAkun().Show();
+            this.Hide();
+        }
+
+        private void gajiRadio3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
